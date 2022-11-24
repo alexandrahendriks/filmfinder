3c8251dac4fea10a556e870a72352e6d2c317d59
Alexandra Hendriks <kolozsvari.alexandra25@gmail.com>
2022-11-24 18:11:13 +0100

    further changes

--- script.js
+++ script.js
@@ -22,9 +22,58 @@ function addMoviesToDom(movies) {
 }
 addMoviesToDom(movies); 
 
+const allRadioButtons = document.querySelectorAll('input[name="film-type"]');
+console.log(allRadioButtons);
+
+function addEvenentListeners(buttons){
+     for(i=0; i < buttons.length; i++) {
+        buttons[i].onchange = function() {
+            console.log(this.value);
+            if (buttons != null) {
+                //console.log(" radio button checked");
+            }
+        }
+    } 
+}
+addEvenentListeners(allRadioButtons);
 
 
+function handleOnChangeEvent(event) {
+    switch (event.target.value) {
+        case "new-movies":
+            console.log("New movies");
+            break;
+        case "avenger":
+            console.log("Avenger movies");
+            
+            break;
+        case "x-men":
+            console.log("X-men movies");
+            break;
+        case "princess":
+            console.log("Princess movies");
+            filterMovies(event.target.value);
+            break;
+        case "batman":
+            console.log("Batman movies");
+            break;
+    }
+}
 
+function filterMovies(word){
+
+    movies.filter((movie) => {
+        if (movie.title("princess") === true) {
+            console.log(movie.title)
+        }    
+})
+}
+filterMovies(movies)
+
+
+const filteredItems = items.filter((item) => {
+    return item.price <= 100
+})
 
 
 
